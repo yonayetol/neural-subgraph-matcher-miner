c46f34e1f817d45f1248e4e91a621918a94abea6
abel ashenafi <ashenafiabel66@gmail.com>
2025-07-23 20:21:16 +0300

    Cleaning the sampling method

--- subgraph_mining/decoder.py
+++ subgraph_mining/decoder.py
@@ -224,7 +224,6 @@ def pattern_growth(dataset, task, args):
                             subgraph.edges[mapping[old_u], mapping[old_v]].update(attrs)
                         
                         subgraph.add_edge(0, 0)
-                        subgraph.nodes[0]["anchor"] = True
                         neighs.append(subgraph)
                         if args.node_anchored:
                             anchors.append(0)
@@ -237,7 +236,6 @@ def pattern_growth(dataset, task, args):
                 neigh = graph.subgraph(neigh)
                 neigh = nx.convert_node_labels_to_integers(neigh)
                 neigh.add_edge(0, 0)
-                neigh.nodes[0]["anchor"] = True
                 neighs.append(neigh)
                 if args.node_anchored:
                     anchors.append(0)
